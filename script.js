442fafdd19fb081982fa5a61a39ac64ee1b42745
Matt <mrmace@umail.iu.edu>
2018-04-25 19:57:53 -0400

    Player Rotation

--- script.js
+++ script.js
@@ -56,6 +56,41 @@ function playerMove(dir) {
     }
 }
 
+function rotate(matrix, dir){
+    for(let y = 0; y< matrix.length; ++y){
+        for(let x = 0; x<y; ++x){
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+    if(dir > 0){
+        matrix.forEach(row => row.reverse());
+    }else{
+        matrix.reverse();
+    }
+}
+
+function playerRotate(dir) {
+      const pos = player.pos.x;
+    let offset = 1;
+        rotate(player.matrix, dir);
+        while (collide(arena, player)) {
+              player.pos.x += offset;
+                offset = -(offset + (offset > 0 ? 1 : -1));
+               if (offset > player.matrix[0].length) {
+                     rotate(player.matrix, -dir);
+                       player.pos.x = pos;
+                       return;
+                    }
+            }
+    }
+
+
 function merge(arena, player){
     player.matrix.forEach((row,y ) => {
         row.forEach((value,x ) => {
@@ -112,7 +147,12 @@ document.addEventListener('keydown', event => {
         // player.pos.x++;
 }else if(event.keyCode === 40){
        playerDrop();
+}else if(event.keyCode === 81){
+        playerRotate(-1)
+}else if(event.keyCode === 87) {
+        playerRotate(1);
 }
+
 })
 
 update();
