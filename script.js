5a77ef3612ea2c10be56cc6ca075de8c29532ad6
Matt <mrmace@umail.iu.edu>
2018-04-25 22:57:29 -0400

    Color Change on spawned blocks

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ const matrix = [
     [0, 1, 0]
 ];
 
+const colors = [
+    null,
+    'red',
+    'blue',
+    'purple',
+    'green',
+    'yellow',
+    'orange',
+    'violet',
+    'pink',
+    'gold',
+    'white'
+]
+
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y) {
@@ -31,7 +45,8 @@ function drawMatrix(matrix, offset) {
         if(value !== 0
 )
     {
-        context.fillStyle = 'red';
+        // context.fillStyle = colors[Math.floor(Math.random()* colors.length)] ;
+        context.fillStyle = colors[value] ;
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
     }
 })
@@ -141,38 +156,38 @@ function createBlock(type) {
         ];
     } else if (type === 'O') {
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
         ];
     } else if (type === 'L') {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
         ];
     } else if (type === 'J') {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0],
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
         ];
     } else if (type === 'I') {
         return [
-            [0, 1, 0,0],
-            [0, 1, 0,0],
-            [0, 1, 0,0],
-            [0, 1, 0,0],
+            [0, 5, 0,0],
+            [0, 5, 0,0],
+            [0, 5, 0,0],
+            [0, 5, 0,0],
         ];
     } else if (type === 'S') {
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0],
         ];
     } else if (type === 'Z') {
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0],
         ];
     }
